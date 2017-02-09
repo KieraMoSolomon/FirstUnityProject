616b1a6186e16479997f5ebeb680df6cc682421e
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-09 11:55:42 -0700

    Finished the code for ifStatements

--- New Unity Project/Assets/ifStatements.cs
+++ New Unity Project/Assets/ifStatements.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class ifStatements : MonoBehaviour {
 	private float myCash = 20.75f;
 	private int noMoney = 0;
+	private int someMoney = 10;
 	private string store = "Babies R Us";
 	private bool shopping = false;
 	private bool work = false;
+	private float husbandCash = 200.33f;
 
 	/*
 	 * ==
@@ -36,17 +38,22 @@ public class ifStatements : MonoBehaviour {
 		if (work == true) {
 			print ("Work sucks, at least you get more money.");
 			myCash = 50;
+			work = false;
 		}
 		if (myCash < noMoney) {
 			print ("You spent more than you had.");
 		}
-		if (){
+		if (myCash >= someMoney){
+			print ("You still have enough to spend more.");
 		}
-		if (){
+		if (store != "Babies R Us"){
+			print ("Target is a bad place that you always over spend at, you better go to Walmart...");
 		}
-		if () {
+		if (husbandCash > myCash) {
+			print ("Husband has more money than I do, I should ask him for some.");
 		}
-		if () {
+		if (husbandCash < myCash) {
+			print ("Well if he has less than I do, we are too poor for me to go shopping.");
 		}
 	}
 }
