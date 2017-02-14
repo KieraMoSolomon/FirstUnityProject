47f3521b71c745a638fd8ba2d03ae363df91204c
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-14 10:25:55 -0700

    Added ifElseStatement file

---
+++ New Unity Project/Assets/ifElseStatements.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifElseStatements : MonoBehaviour {
+	private bool awake = false;
+	// Use this for initialization
+	void Start () {
+		if (awake == false) {
+			print ("WAKE UP!");
+		}
+		else {
+			print ("Go to sleep!");
+		}
+
+	}
+}
