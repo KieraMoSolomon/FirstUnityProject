40780351314bcbe82ef242e2234bdb181c988d36
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-16 10:55:28 -0700

    example with different animals

--- New Unity Project/Assets/switchStatements.cs
+++ New Unity Project/Assets/switchStatements.cs
@@ -3,8 +3,47 @@ using System.Collections;
 
 public class switchStatements : MonoBehaviour {
 	private int time = 10;
+	private string animal = "alligator";
+	private string phrase;
+	private bool exist = true;
+
 	// Use this for initialization
 	void Start () {
+		switch (animal) 
+		{
+		case "giraffe":
+			phrase = "wheezy neighbaaaaaah";
+			break;
+		case "penguin":
+			phrase = "Morgan Freeman";
+			break;
+		case "lion":
+			phrase = "roar";
+			break;
+		case "fox":
+			phrase = "adhflknviehflnsjkh";
+			break;
+		case "alligator":
+			phrase = "would you like some tea with that, sir?";
+			break;
+		default:
+			exist = false;
+			break;
+		}
+
+
+		if (exist) {
+			print ("A " + animal + " says \"" + phrase + "\".");
+		} else {
+			print (animal + " does not exist.");
+		}
+
+
+
+
+
+
+
 		//create a switch statement for time of day
 		switch (time) {
 		case 9:
