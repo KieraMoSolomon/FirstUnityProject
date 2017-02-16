dcee5dc503ad7a40632fb3b8a5cd277b16d1e086
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-15 22:24:34 -0700

    Changed the statements for the assignment

--- New Unity Project/Assets/ifElseStatements.cs
+++ New Unity Project/Assets/ifElseStatements.cs
@@ -2,12 +2,28 @@
 using System.Collections;
 
 public class ifElseStatements : MonoBehaviour {
-	//DONT FORGET TO WRITE COMMENTS ABOUT ELSE STATEMENTS
-	private ;
+	//Else if statements will run in order until it finds the true one and will do what that statement says to.
+	//Else statements go at the end of the block of code that you want and will run when the if and else if are false.
+	private string day = Wednesday;
 
 	// Use this for initialization
 	void Start () {
-		if (){
+		if (day == Sunday) {
+			print ("Today is Sunday");
+		} else if (day == Monday) {
+			print ("Today is Monday");
+		} else if (day == Tuesday) {
+			print ("Today is Tuesday");
+		} else if (day == Wednesday) {
+			print ("Today is Wednesday");
+		} else if (day == Thursday) {
+			print ("Today is Thursday");
+		} else if (day == Friday) {
+			print ("Today is Friday");
+		} else if (day == Saturday) {
+			print ("Today is Saturday");
+		} else {
+			print ("Wow there must be a new day");
 		}
 	}
 }
