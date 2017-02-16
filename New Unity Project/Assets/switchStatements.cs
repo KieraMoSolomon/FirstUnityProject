7a835fbaa47ee2abd7b259940f1e9422d3c0afdb
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-16 10:33:34 -0700

    Example of switch statements (time)

--- New Unity Project/Assets/switchStatements.cs
+++ New Unity Project/Assets/switchStatements.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class switchStatements : MonoBehaviour {
-
+	private int time = 10;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		//create a switch statement for time of day
+		switch (time) {
+		case 9:
+			print ("Good morning");
+			print ("It's pleasantly brisk outside.");
+			break;
+		case 12:
+			print ("Lunch time!");
+			break;
+		case 18:
+			print ("Time to go home");
+			break;
+		default:
+			print ("Get back to work!");
+			break;
+		}
 	}
 }
