2630cb56e65e5047fb9b8bf1eb1b41d55f5e43d2
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-17 23:24:44 -0700

    Worked on switch statements added 3 examples

--- New Unity Project/Assets/switchStatements.cs
+++ New Unity Project/Assets/switchStatements.cs
@@ -6,9 +6,73 @@ public class switchStatements : MonoBehaviour {
 	private string animal = "alligator";
 	private string phrase;
 	private bool exist = true;
+	//enemy attack
+	private string enemy = "monster";
+	private int health = 100;
+	private string phrase2;
+	//factory
+	private string product = "doll";
+	//gardner
+	private string season = "summer";
+	private string phrase3;
+	private bool garden = true;
+	//
+
 
 	// Use this for initialization
 	void Start () {
+		switch (enemy) {
+		case "monster":
+			health = 92;
+			phrase2 = "lost 8 health";
+			break;
+		case "zombie":
+			health = 95;
+			phrase2 = "lost 5 health";
+			break;
+		case "werewolf":
+			health = 80;
+			phrase2 = "lost 20 health";
+			break;
+		default:
+			health = 100;
+			phrase2 = "no monsters came"
+				break;
+		}
+		if (health <= 100) {
+			print (phrase2);
+		}
+
+		switch(product){
+		case "nintendo":
+			print ("We are out of stock of those");
+			break;
+		case "doll":
+			print ("Making more as we speak");
+			break;
+		default:
+			print ("You can make a request for a toy here");
+			break;
+		}
+
+		switch (season){
+		case "Winter":
+			phrase3 = "You don't harvest anything";
+			break;
+		case "Spring":
+			phrase3 = "You can plant some vegetables";
+			break;
+		case "Summer":
+			phrase3 = "The tulips look pretty";
+			break;
+		default:
+			phrase3 = "Fall is when everything starts to die";
+			break;
+		}
+		if (garden){
+			print (phrase3);
+		}
+
 		switch (animal) 
 		{
 		case "giraffe":
@@ -37,12 +101,6 @@ public class switchStatements : MonoBehaviour {
 		} else {
 			print (animal + " does not exist.");
 		}
-
-
-
-
-
-
 
 		//create a switch statement for time of day
 		switch (time) {
