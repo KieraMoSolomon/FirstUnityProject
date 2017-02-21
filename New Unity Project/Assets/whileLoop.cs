b1ca227f5f39a963a3bd9ebd90b23c87a8ad4279
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-21 10:52:52 -0700

    gameOn as condition

--- New Unity Project/Assets/whileLoop.cs
+++ New Unity Project/Assets/whileLoop.cs
@@ -4,9 +4,24 @@ using System.Collections;
 public class whileLoop : MonoBehaviour {
 	private int bottlesOfPop = 0;
 	public int maxBottles = 10;
+
+	private bool gameOn = true;
 	// Use this for initialization
 	void Start () {
-		while (bottlesOfPop < maxBottles) {
+		while(gameOn){
+			print (bottlesOfPop + " bottles of Pop on the wall.");
+			bottlesOfPop++;
+			if (bottlesOfPop == maxBottles) {
+				gameOn = false;
+			}
+		}
+
+
+
+
+
+
+		/*while (bottlesOfPop < maxBottles) {
 			if (bottlesOfPop % 2 == 0) {
 				print (bottlesOfPop + " bottles of Pop on the wall.");
 			}
@@ -14,5 +29,6 @@ public class whileLoop : MonoBehaviour {
 			bottlesOfPop = bottlesOfPop + 2;
 		}
 		print ("All done");
+*/
 	}
 }
