f1d49e5836e4a18389af0d31200c0f6462653b31
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-23 12:34:59 -0700

    added the different arrays that will exist

--- New Unity Project/Assets/array.cs
+++ New Unity Project/Assets/array.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class array : MonoBehaviour {
 	private string adj1;
+	private string adj2;
+	private string animal;
+	private string liquid;
+	private string adj3;
+	private string song;
+	private string noun1;
+	private string adj4;
+	private string place;
+	private string food;
+	private string verb;
+	private string adj5;
 
 	// Use this for initialization
 	void Start () {
