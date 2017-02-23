6adda6ed6452736cd835ceb7697547afb07a2625
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-23 14:22:54 -0700

    got the assignment to work

--- New Unity Project/Assets/array.cs
+++ New Unity Project/Assets/array.cs
@@ -2,28 +2,27 @@
 using System.Collections;
 
 public class array : MonoBehaviour {
-	private string adj1;
-	private string adj2;
-	private string animal;
-	private string liquid;
-	private string adj3;
-	private string song;
-	private string noun1;
-	private string adj4;
-	private string place;
-	private string food;
-	private string verb;
-	private string adj5;
+	private string[] adj1 = {"happy", "wonderful", "annoying", "silly", "weird", "crazy"};
+	private string[] adj2 = {"green", "lazy", "fat", "hungry", "stupid", "cute", "slimy"};
+	private string[] animal = {"monkey", "pidgeon", "lion", "sloth", "duck", "gecko", "llama", "frog", "goat", "pig"};
+	private string[] liquid = {"chocolate syrup", "water", "soad", "pudding", "juice", "milk"};
+	private string[] adj3 = {"crunchy", "soft", "warm", "freezing", "purple"};
+	private string[] song = {"Once Upon a Dream", "Everything is Fine", "Why don't we just dance", "Turn the Beat Around", "Popular", "Satisfied"};
+	private string[] noun1 = {"elephant", "buggy", "wagon", "bus", "shopping cart", "car"};
+	private string[] adj4 = {"bumpy", "winding", "dirt", "shinny", "clean", "joyful", "small"};
+	private string[] place = {"library", "store", "police station", "church", "restuarant", "post office"};
+	private string[] food = {"spaghetti", "meatloaf", "roast", "chocolate", "popcorn", "hamburgers", "salad", "mashed potatoes"};
+	private string[] verb = {"jumped", "stabbed", "kissed", "shot", "hugged"};
+	private string[] adj5 = {"handsome", "ugly", "strange", "creepy"};
 
 	// Use this for initialization
 	void Start () {
-		/*Sleeping Beauty was having a very good dream. It was so (adj1) that she was
-		 * giggling in her sleep. A fluffy kitty and a (adj2) (animal) were surfing on
-		 * a sea of (liquid). They crashed onto the (adj3) beach and started doing an
-		 * upbeat dance to their very favorite song, (song). Then, they drove a pink
-		 * (noun) down the (adj4) road to the (place). They ordered some pizza and (food)
-		 * for lunch. Kitty laughed and got (food) all over her fur. Suddenly, Sleeping
-		 * Beauty woke up! She had been (verb) by a (adj5) prince.
-		*/
+
+		print ("Sleeping Beauty was having a very good dream. It was so " + adj1[Random.Range(0, adj1.Length)] + " that she was giggling in her sleep. " +
+			"\n A fluffy kitty and a " + adj2[Random.Range(0, adj2.Length)] + " " + animal[Random.Range(0, animal.Length)] + " were surfing on a sea of " + liquid[Random.Range(0, liquid.Length)] + ". They crashed onto the " + adj3[Random.Range(0, adj3.Length)] + " beach and started doing\n an upbeat dance to their very favorite song, " + song[Random.Range(0, song.Length)] + ". Then, they drove a pink " + noun1[Random.Range(0, noun1.Length)] + 
+			"\n down the " + adj4[Random.Range(0, adj4.Length)] + " road to the " + place[Random.Range(0, place.Length)] + ". They ordered some pizza and " + food[Random.Range(0, food.Length)] + 
+			"\n for lunch. Kitty laughed and got " + food[Random.Range(0, food.Length)] + " all over her fur. Suddenly, Sleeping Beauty woke up! She had been " + verb[Random.Range(0, verb.Length)] + 
+			"\n by a " + adj5[Random.Range(0, adj5.Length)] + " prince.");
+		
 	}
 }
