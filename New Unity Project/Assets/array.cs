961da53deb81d628b29b1369db9a532b24fabb86
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-23 12:40:36 -0700

    typed out the story I found

--- New Unity Project/Assets/array.cs
+++ New Unity Project/Assets/array.cs
@@ -17,6 +17,13 @@ public class array : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		/*Sleeping Beauty was having a very good dream. It was so (adj1) that she was
+		 * giggling in her sleep. A fluffy kitty and a (adj2) (animal) were surfing on
+		 * a sea of (liquid). They crashed onto the (adj3) beach and started doing an
+		 * upbeat dance to their very favorite song, (song). Then, they drove a pink
+		 * (noun) down the (adj4) road to the (place). They ordered some pizza and (food)
+		 * for lunch. Kitty laughed and got (food) all over her fur. Suddenly, Sleeping
+		 * Beauty woke up! She had been (verb) by a (adj5) prince.
+		*/
 	}
 }
