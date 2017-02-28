59646e5ffd9823c5fefa14de7fbe121a6a81829a
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-02-28 11:22:08 -0700

    Added file and scene for listHomework

---
+++ New Unity Project/Assets/listHomework.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class listHomework : MonoBehaviour {
+	public string animal;
+	public List<string> zooRoster = new List<string>();
+	// Use this for initialization
+	void Start () {
+		zooRoster.Add ("monkey");
+		zooRoster.Add ("elephant");
+		zooRoster.Add ("alligator");
+	}
+	public void AddAnimal()
+	{
+		if (animal != "") {
+			if (zooRoster.Contains (animal) != true) {
+				zooRoster.Add (animal);
+			}
+		}
+	}
+}
