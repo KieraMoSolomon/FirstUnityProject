7e4dae6f43b5f54ff87a4d3856758a3b62500ee0
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-03-30 10:43:57 -0600

    Start the first extra credit assignment

---
+++ New Unity Project/Assets/ExtraCreditAssignment.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraCreditAssignment : MonoBehaviour {
+	public float health1 = 4.6f;
+	public float health2 = 10.0f;
+	public float health3 = 1.1f;
+
+	public float Mult(){
+		float temp = health1 * health2 * health3;
+		return temp;
+	}
+	// Use this for initialization
+	void Start () {
+
+
+	}
+}
