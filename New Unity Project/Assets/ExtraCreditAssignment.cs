ba475a1df4aa01c008bc1a2cecb57049d623db9c
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-03-30 11:14:50 -0600

    Finished the first function exercise

--- New Unity Project/Assets/ExtraCreditAssignment.cs
+++ New Unity Project/Assets/ExtraCreditAssignment.cs
@@ -6,13 +6,13 @@ public class ExtraCreditAssignment : MonoBehaviour {
 	public float health2 = 10.0f;
 	public float health3 = 1.1f;
 
-	public float Mult(){
+	private float Mult(float health1, float health2, float health3){
 		float temp = health1 * health2 * health3;
 		return temp;
 	}
 	// Use this for initialization
 	void Start () {
-
-
+		float healthPrint = Mult(health1, health2, health3);
+		print (healthPrint);
 	}
 }
